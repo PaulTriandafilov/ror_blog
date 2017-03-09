0bf4e04d86e721b171849aa5db54f93da31cb551
pavel <pavel.triandafilov@castle.co>
2017-03-09 23:12:51 +0400

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+blogs = Blog.create([{ name: 'Blog 1' }, {name: 'Blog 2'}])
+
+blogs.each do |blog|
+  articles = Article.create([ {title: "Article for #{blog.name} #1", text: "Text for Article for blog #{blog.name}", blog: blog}, 
+                              {title: "Article for #{blog.name} #2", text: "Text for Article for blog #{blog.name}", blog: blog} 
+                            ])
+end
+
+Article.new(title: 'Unassigned article 1', text: 'Some text').save(validate: false)
+Article.new(title: 'Unassigned article 2', text: 'Some text').save(validate: false)
