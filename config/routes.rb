81477cd8a1ef45a994461ebc5030ea393102d589
pavel <pavel.triandafilov@castle.co>
2017-03-09 09:27:49 +0400

    Add list of all articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 
-  root 'welcome#index'
+  root 'blogs#index'
 
   resources :blogs do
     resources :articles do
       resources :comments
     end
   end
+
+  get 'articles', to: 'articles#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
